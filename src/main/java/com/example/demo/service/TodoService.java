b3e33237d5e5357da572094a2169186c2620c750
Yun Seo Bin <sb030110@sookmyung.ac.kr>
2023-06-20 21:11:48 +0900

    백엔드 개발 17. @Repository 에서 쿼리 추가하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -14,12 +14,11 @@ public class TodoService {
 	
 	public String testService() {
 		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-		
+		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
 		// Todo Entity 저장
 		repository.save(entity);
 		// Todo Entity 검색
-		TodoEntity saveEntity = repository.findById(entity.getId()).get();
-		return saveEntity.getTitle();
+		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+		return savedEntity.getUserId();
 	}
 }
